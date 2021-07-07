9cdb78c87e8e09447cf440e212a48e5c6518f736
Jack Ceroni <jackceroni@gmail.com>
2021-07-07 12:43:07 -0400

    changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='autohf',
+    version='0.0.1',
+    packages=['autohf'],
+    url='',
+    license='',
+    author='jackceroni',
+    author_email='jackceroni@gmail.com',
+    description='Automatic differentiation + Hartree-Fock',
+)
