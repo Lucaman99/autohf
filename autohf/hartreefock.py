104abfc187e5939349ccb0f8d25beb757a149668
Jack Ceroni <jackceroni@gmail.com>
2021-07-28 15:13:52 -0400

    precision error

--- autohf/hartreefock.py
+++ autohf/hartreefock.py
@@ -120,6 +120,73 @@ def fock_matrix(num_elec, charge, coeffs, atomic_orbitals):
         return F
     return fock
 
+###########################################################
+##################### DELETE ASAP!!!! #####################
+###########################################################
+
+
+from functools import partial
+import numpy.linalg as npla
+from autograd.numpy.numpy_wrapper import wrap_namespace
+from autograd.numpy import numpy_wrapper as wrap_anp
+from autograd.extend import defvjp
+
+wrap_namespace(npla.__dict__, globals())
+
+
+def T(x): return wrap_anp.swapaxes(x, -1, -2)
+
+
+_dot = partial(wrap_anp.einsum, '...ij,...jk->...ik')
+
+# batched diag
+_diag = lambda a: wrap_anp.eye(a.shape[-1]) * a
+
+
+def grad_eigh(ans, x, UPLO='L'):
+    """Gradient for eigenvalues and vectors of a symmetric matrix."""
+    N = x.shape[-1]
+    w, v = ans  # Eigenvalues, eigenvectors.
+    vc = wrap_anp.conj(v)
+
+    def vjp(g):
+        wg, vg = g  # Gradient w.r.t. eigenvalues, eigenvectors.
+        w_repeated = wrap_anp.repeat(w[..., wrap_anp.newaxis], N, axis=-1)
+
+        # Eigenvalue part
+        vjp_temp = _dot(vc * wg[..., wrap_anp.newaxis, :], T(v))
+
+        # Add eigenvector part only if non-zero backward signal is present.
+        # This can avoid NaN results for degenerate cases if the function depends
+        # on the eigenvalues only.
+        if wrap_anp.any(vg):
+            off_diag = wrap_anp.ones((N, N)) - wrap_anp.eye(N)
+            d = (T(w_repeated) - w_repeated + wrap_anp.eye(N))
+            divisor = wrap_anp.where(d == 0, 1e-16, d)
+            F = off_diag / divisor
+            vjp_temp += _dot(_dot(vc, F * _dot(T(v), vg)), T(v))
+
+        # eigh always uses only the lower or the upper part of the matrix
+        # we also have to make sure broadcasting works
+        reps = wrap_anp.array(x.shape)
+        reps[-2:] = 1
+
+        if UPLO == 'L':
+            tri = wrap_anp.tile(wrap_anp.tril(wrap_anp.ones(N), -1), reps)
+        elif UPLO == 'U':
+            tri = wrap_anp.tile(wrap_anp.triu(wrap_anp.ones(N), 1), reps)
+
+        return wrap_anp.real(vjp_temp) * wrap_anp.eye(vjp_temp.shape[-1]) + \
+               (vjp_temp + wrap_anp.conj(T(vjp_temp))) * tri
+
+    return vjp
+
+
+defvjp(eigh, grad_eigh)
+
+
+###########################################################
+
 
 def hartree_fock(num_elec, charge, atomic_orbitals, tol=1e-8):
     """Performs the Hartree-Fock procedure
@@ -138,19 +205,20 @@ def hartree_fock(num_elec, charge, atomic_orbitals, tol=1e-8):
         F_initial = H_core
 
         # Builds the X matrix
-        v, w = anp.linalg.eigh(S)
+        v, w = eigh(S)
         v = anp.array([1 / anp.sqrt(r) for r in v])
         diag_mat, w_inv = anp.diag(v), w.T
         X = w @ diag_mat @ w_inv
 
         # Constructs F_tilde and finds the initial coefficients
         F_tilde_initial = X.T @ F_initial @ X
-        v_fock, w_fock = anp.linalg.eigh(F_tilde_initial)
+        v_fock, w_fock = eigh(F_tilde_initial)
 
         coeffs = X @ w_fock
         P = density_matrix(num_elec, coeffs)
 
         counter = 0
+
         while not self_consistent:
             JM = anp.einsum('pqrs,rs->pq', eri_tensor, P)
             KM = anp.einsum('psqr,rs->pq', eri_tensor, P)
@@ -160,7 +228,7 @@ def hartree_fock(num_elec, charge, atomic_orbitals, tol=1e-8):
             F_tilde = X.T @ F @ X
 
             # Solve for eigenvalues and eigenvectors
-            v_fock, w_fock = anp.linalg.eigh(F_tilde)
+            v_fock, w_fock = eigh(F_tilde)
             w_fock = X @ w_fock
             P_new = density_matrix(num_elec, w_fock)
 
